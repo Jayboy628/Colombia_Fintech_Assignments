2ec24ea69a26bff606a9179bb23a28854d1ba6a1
Jayboy628 <shaunjay.brown@gmail.com>
2021-08-23 14:15:27 -0400

    python02

--- 19-Blockchain-Python/BlockchainPython-Project/Starter-Code/wallet/wallet.py
+++ 19-Blockchain-Python/BlockchainPython-Project/Starter-Code/wallet/wallet.py
@@ -53,3 +53,48 @@ def priv_key_to_account(coin, priv_key):
 
 eth_account = priv_key_to_account(ETH, eth_priv_key)
 btc_account = priv_key_to_account(BTCTEST, btc_priv_key)
+
+# Create a function called `priv_key_to_account` that converts privkey strings to account objects.
+def priv_key_to_account(coin, priv_key):
+    if coin == ETH:
+        return Account.privateKeyToAccount(priv_key)
+    if coin == BTCTEST:
+        return PrivateKeyTestnet(priv_key)
+    
+eth_account = priv_key_to_account(ETH,eth_priv_key)
+btc_account = priv_key_to_account(BTCTEST,btc_priv_key)
+
+# Create a function called `create_tx` that creates an unsigned transaction appropriate metadata.
+def create_tx(coin, account, recipient,amount):
+    # YOUR CODE HERE
+    global tx_data
+    if coin.upper() == 'ETH':
+        gasEstimate = w3.eth.estimateGas(
+        {"from": account.address, "to": recipient, "value": amount}
+        )
+        tx_data = {
+            "to": recipient,
+            "from": account.address,
+            "value": amount,
+            "gasPrice": w3.eth.gasPrice,
+            "gas": gasEstimate,
+            "nonce": w3.eth.getTransactionCount(account.address)
+        }
+        return tx_data
+    
+    if coin.upper() == 'BTCTEST':
+        return PrivateKeyTestnet.prepare_transaction(account.address, [(recipient, amount, BTC)])
+# Create a function called `send_tx` that calls `create_tx`, signs and sends the transaction.
+def send_tx(coin, account, recipient,amount):
+    if coin.upper() == 'ETH':
+        tx_eth = create_tx(coin, account, recipient, amount)
+        sign = account.signTrasaction(tx_eth)
+        result = w3.eth.sendRawTransaction(sign.rawTransaction)
+        print(result.hex())
+        return result.hex()
+    else:
+        tx_btctest = create_tx(coin, account, recipient, amount)
+        sign_tx_btctest = account.sign_transaction(tx_btctest)
+        from bit.network import NetworkAPI
+        NetworkAPI.broadcast_tx_testnet(sign_tx_btctest)
+        return sign_tx_btctest
